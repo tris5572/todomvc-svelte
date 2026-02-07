f38f04a0a5e5d6e6fe484d1c0c6f0ffbb37fb7ac
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-02-08 05:29:30 +0900

    feat: 削除ボタンを実装

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,18 @@
     { id: 1, title: "Example Todo Item", completed: false },
     { id: 2, title: "サンプルtodo", completed: true },
   ]);
+
+  /** 指定した ID の TODO アイテムを削除する */
+  function deleteTodo(id: number) {
+    todos = todos.filter((t) => t.id !== id);
+  }
 </script>
 
 <main>
   <div>
     <!-- `#each todos as todo` だと、todo が一時的な参照になって更新できないためインデックスアクセスする -->
     {#each todos as _, i}
-      <Item bind:todo={todos[i]} />
+      <Item bind:todo={todos[i]} onDelete={deleteTodo} />
     {/each}
   </div>
 </main>
--- src/lib/Item.svelte
+++ src/lib/Item.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
   import type { Todo } from "./types";
 
-  let { todo = $bindable() }: { todo: Todo } = $props();
+  let {
+    todo = $bindable(),
+    onDelete,
+  }: { todo: Todo; onDelete: (id: number) => void } = $props();
 </script>
 
 <div>
   <input type="checkbox" bind:checked={todo.completed} />
   {todo.title}
+  <button onclick={() => onDelete(todo.id)}>✗</button>
 </div>
