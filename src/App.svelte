66323eab039cfdc4308948c26954cc5661869427
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-02-07 17:57:33 +0900

    feat: Todoアイテムの表示を実装

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,21 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import Item from "./lib/Item.svelte";
+  import type { Todo } from "./lib/types";
+
+  let todos = $state<Todo[]>([
+    { id: 1, title: "Example Todo Item", completed: false },
+    { id: 2, title: "サンプルtodo", completed: true },
+  ]);
 </script>
 
 <main>
   <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <!-- `#each todos as todo` だと、todo が一時的な参照になって更新できないためインデックスアクセスする -->
+    {#each todos as _, i}
+      <Item bind:todo={todos[i]} />
+    {/each}
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
---
+++ src/lib/Item.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { Todo } from "./types";
+
+  let { todo = $bindable() }: { todo: Todo } = $props();
+</script>
+
+<div>
+  <input type="checkbox" bind:checked={todo.completed} />
+  {todo.title}
+</div>
