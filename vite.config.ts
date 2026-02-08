0beb63c205f0d4a9911b4461f078fb0794d4885d
tris5572 <68403260+tris5572@users.noreply.github.com>
2026-02-09 05:00:07 +0900

    chore: vitest を導入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+// import { defineConfig } from 'vite'
+import { defineConfig } from "vitest/config";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  resolve: process.env.VITEST
+    ? {
+        conditions: ["browser"],
+      }
+    : undefined,
+});
